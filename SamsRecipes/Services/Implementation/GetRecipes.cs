08bbdaa90af82b411d759cb5c50d178a2c096f5d
Sam Armstrong <Sam.Armstrong@sorted.com>
2023-08-26 01:23:37 +0100

    Formatting

--- SamsRecipes/Services/Implementation/GetRecipes.cs
+++ SamsRecipes/Services/Implementation/GetRecipes.cs
@@ -5,10 +5,12 @@ namespace SamsRecipes.Services.Implementation
     public class GetRecipes : IGetRecipes
     {
         private readonly IExtract _extract;
+
         public GetRecipes(IExtract extractFromFolder)
         {
             _extract = extractFromFolder;
         }
+
         public List<Recipe> Get()
         {
             var filesInDirectory = Directory.GetFiles(@"./Recipes");
@@ -16,7 +18,8 @@ namespace SamsRecipes.Services.Implementation
             var recipes = new List<Recipe>();
 
             foreach (var recipePath in filesInDirectory)
-            {   var jsonRecipe = _extract.ExtractFile(recipePath);
+            {
+                var jsonRecipe = _extract.ExtractFile(recipePath);
                 recipes.Add(JsonSerializer.Deserialize<Recipe>(jsonRecipe));
             }
 
